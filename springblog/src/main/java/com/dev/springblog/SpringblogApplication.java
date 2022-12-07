75d390ba0f8607aac4c15b2af828ee9f80571a0e
Akhil Ganesh - <akhilganesh@dxc.com>
2022-12-07 22:07:02 +0530

    removed creds

--- springblog/src/main/java/com/dev/springblog/SpringblogApplication.java
+++ springblog/src/main/java/com/dev/springblog/SpringblogApplication.java
@@ -2,7 +2,7 @@ package com.dev.springblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test comment
+
 @SpringBootApplication
 public class SpringblogApplication {
 
